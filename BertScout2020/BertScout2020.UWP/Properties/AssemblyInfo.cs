14d193d612285eceee11e89ea76a8a18343daa6f
Scott Bakker <scott@bakkergames.com>
2020-01-23 20:32:09 -0500

    Updated icon

--- BertScout2020/BertScout2020.UWP/Properties/AssemblyInfo.cs
+++ BertScout2020/BertScout2020.UWP/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.10.30.2251")]
-[assembly: AssemblyFileVersion("2019.10.30.2251")]
+[assembly: AssemblyVersion("2020.1.23.1956")]
+[assembly: AssemblyFileVersion("2020.1.23.1956")]
 [assembly: ComVisible(false)]
