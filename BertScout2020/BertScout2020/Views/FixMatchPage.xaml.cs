e7285ec369fcb5bf6b20559ff28598094c694b5a
Chloe Falkner <chloefalkner@gmail.com>
2020-02-08 12:21:12 -0500

    Created FixMatchPage

---
+++ BertScout2020/BertScout2020/Views/FixMatchPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BertScout2020.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FixMatchPage : ContentPage
+    {
+        public FixMatchPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
