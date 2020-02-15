8377b2f84e0aa7af295932732726ec2ae7be47af
Scott Bakker <scott@bakkergames.com>
2020-02-15 12:12:24 -0500

    Working on pull kindle data/pix

--- GetKindleData/GetKindleData.bat
+++ GetKindleData/GetKindleData.bat
@@ -1,3 +1,4 @@
 @echo off
-%LOCALAPPDATA%\Android\sdk\platform-tools\adb.exe pull /storage/sdcard0/Documents D:\BERT\BertScout2020Data
+%LOCALAPPDATA%\Android\sdk\platform-tools\adb.exe pull /storage/sdcard0/Documents D:\Projects\BertScout2020\KindleData
+%LOCALAPPDATA%\Android\sdk\platform-tools\adb.exe pull /storage/sdcard0/DCIM/Camera D:\Projects\BertScout2020\KindleData
 pause
