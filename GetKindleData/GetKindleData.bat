cf03e3be3b0874db61c7c93b9a1630ba842f0e47
Scott Bakker <scott@bakkergames.com>
2020-02-16 14:42:45 -0500

    got proper adb path

--- GetKindleData/GetKindleData.bat
+++ GetKindleData/GetKindleData.bat
@@ -1,4 +1,4 @@
 @echo off
-%LOCALAPPDATA%\Android\sdk\platform-tools\adb.exe pull /storage/sdcard0/Documents D:\Projects\BertScout2020\KindleData
-%LOCALAPPDATA%\Android\sdk\platform-tools\adb.exe pull /storage/sdcard0/DCIM/Camera D:\Projects\BertScout2020\KindleData
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" pull /storage/sdcard0/Documents D:\Projects\BertScout2020\KindleData
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" pull /storage/sdcard0/DCIM/Camera D:\Projects\BertScout2020\KindleData
 pause
