56fb5ec712c0bcb21e09531990dd0f464b7ddb45
Scott Bakker <sbakker343@gmail.com>
2021-11-17 23:13:09 -0500

    20211117

--- GetKindleData/GetKindleData.bat
+++ GetKindleData/GetKindleData.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" pull /storage/sdcard0/Documents D:\Projects\BertScout2020\KindleData
-"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" pull /storage/sdcard0/DCIM/Camera D:\Projects\BertScout2020\KindleData
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" pull /storage/sdcard0/Documents C:\Projects\BertScout2020\KindleData
+"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe" pull /storage/sdcard0/DCIM/Camera C:\Projects\BertScout2020\KindleData
 pause
